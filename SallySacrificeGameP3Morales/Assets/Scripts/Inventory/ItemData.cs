42cc107d0ffd50486aaada317b731771a92fe46c
Bitt3rcrawlANA <722697@student.dusd.net>
2025-12-01 12:01:00 -0800

    prepped workspace
    not much done. set up item data script (sort of)

---
+++ SallySacrificeGameP3Morales/Assets/Scripts/Inventory/ItemData.cs
@@ -0,0 +1,19 @@
+using JetBrains.Annotations;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Item Data")]
+public class ItemData : ScriptableObject
+{
+    // Start is called once before the first execution of Update after the script is created
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
